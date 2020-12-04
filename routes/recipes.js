57cf4d9a8f2cfdb0f2d76bf123dc56bfb6104ae0
m-ohamed <mohamed.adel-ibrahim@student.guc.edu.eg>
2020-12-04 16:51:55 +0200

    minor changes

--- routes/recipes.js
+++ routes/recipes.js
@@ -6,7 +6,7 @@ const storage = multer.diskStorage(
 {
     destination: (req, file, cb) =>
     {
-        cb(null, './uploads/');
+        cb(null, './public/uploads/');
     },
     filename: (req, file, cb) =>
     {
